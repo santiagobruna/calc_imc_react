530574f1b813c64dfc5d6808fc966ba2d12431a4
bruna <bruna@teste.com>
2025-01-21 20:05:31 -0300

    corrigindo erro

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Formulario from "./components/Formulario";
 import Infos from "./components/Infos";
-import styles from './main.module.scss';
+import styles from "./main.module.scss";
 
 function App() {
   const [heigth, setHeigth] = useState("");
@@ -11,15 +11,16 @@ function App() {
   const [classificationClass, setClassificationClass] = useState("");
 
   const handleHeigthValue = (e) => {
-    const newValueA = e.target.value.replace(',', '.');
+    const newValueA = e.target.value.replace(",", ".");
     setHeigth(newValueA);
   };
   const handleWeigthValue = (e) => {
-    const newValueB = e.target.value.replace(',', '.');
+    const newValueB = e.target.value.replace(",", ".");
     setWeight(newValueB);
   };
   const calcImc = () => {
-    if (heigth && weight) { // Se os valores forem válidos
+    if (heigth && weight) {
+      // Se os valores forem válidos
       const heigthEmMetros = parseFloat(heigth); // Garantir que é um número
       const weightNumber = parseFloat(weight); // Garantir que é um número
 
@@ -36,8 +37,8 @@ function App() {
   };
   const classificationImc = (imcCalculado) => {
     let classification = "";
-    let classificationClass = ""; 
-    if(imcCalculado < 18.5){
+    let classificationClass = "";
+    if (imcCalculado < 18.5) {
       classification = "Abaixo do Peso";
       classificationClass = "alert-warning";
     } else if (imcCalculado >= 18.5 && imcCalculado < 25) {
@@ -51,25 +52,28 @@ function App() {
       classificationClass = "alert-danger";
     }
     setClassification(classification);
-    setClassificationClass(classificationClass); 
-  }
+    setClassificationClass(classificationClass);
+  };
   return (
-    <div className={`${styles.container} container mt-5 p-5 rounded`} >
+    <div className={`${styles.container} container mt-5 p-5 rounded`}>
       <h1>Calcule o seu IMC com react...</h1>
-      Bem-vindo ao nosso site! Aqui, você pode calcular o seu Índice de Massa Corporal (IMC) com facilidade, apenas informando sua altura e peso. Nosso objetivo é oferecer uma maneira simples e rápida para que você saiba mais sobre sua saúde e tome decisões informadas sobre seu bem-estar. Experimente agora e descubra sua classificação de IMC!
-      <Formulario 
-        heigthValue={handleHeigthValue} 
-        weightValue={handleWeigthValue} 
+      Bem-vindo ao nosso site! Aqui, você pode calcular o seu Índice de Massa
+      Corporal (IMC) com facilidade, apenas informando sua altura e peso. Nosso
+      objetivo é oferecer uma maneira simples e rápida para que você saiba mais
+      sobre sua saúde e tome decisões informadas sobre seu bem-estar.
+      Experimente agora e descubra sua classificação de IMC!
+      <Formulario
+        heigthValue={handleHeigthValue}
+        weightValue={handleWeigthValue}
         calcImc={calcImc}
       />
-      
-      {imc !== null  &&  (
+      {imc !== "" && classification && (
         <div>
           <p className="text-dark">O seu IMC é de {imc}</p>
           <p className={`alert ${classificationClass}`}>De acordo com a Organização Mundial da Saúde, sua classificação é {classification}. Para atingir um valor de IMC normal, seu peso deve estar entre 50.4 e 67.8 kg</p>
         </div>
       )}
-      <Infos/>
+      <Infos />
     </div>
   );
 }
